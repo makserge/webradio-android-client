64ed59dd89fab1850e9b1a6823391f61a89a95de
Sergey Makogon <sergey_makogon@epam.com>
2015-11-30 00:00:44 +0200

    Added Toolbar

--- app/src/main/java/com/smsoft/webradio/activity/MainActivity.java
+++ app/src/main/java/com/smsoft/webradio/activity/MainActivity.java
@@ -2,6 +2,8 @@ package com.smsoft.webradio.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.smsoft.webradio.R;
 
@@ -11,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
